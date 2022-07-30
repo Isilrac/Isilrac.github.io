ca961c4e161359f60a3f366aa9a4ae23d50d9d14
Carlos Rodriguez <40615728+Isilrac@users.noreply.github.com>
2022-07-29 20:44:33 -0500

    20:44
    Mockup Opacity Finished

--- js/script.js
+++ js/script.js
@@ -18,3 +18,31 @@ var swiper = new Swiper(".slide-content", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+function revealMockup() {
+
+  var hiFi = document.getElementById("wireframe");
+  var mockUp = document.getElementById("mockup");
+  var headerTwo = document.getElementById("mockupHeaderTwo");
+  var headerThree = document.getElementById("mockupHeaderThree");
+
+  var windowHeight = window.innerHeight;
+
+  var headerTwoTop = headerTwo.getBoundingClientRect().top;
+  var headerThreeTop = headerThree.getBoundingClientRect().top;
+  var elementVisible = 0;
+
+  if (headerTwoTop < windowHeight - elementVisible) {
+    hiFi.classList.add("active");
+  } else {
+    hiFi.classList.remove("active");
+  }
+
+  if (headerThreeTop < windowHeight - elementVisible) {
+    mockUp.classList.add("active");
+  } else {
+    mockUp.classList.remove("active");
+  }
+}
+
+window.addEventListener("scroll", revealMockup);
